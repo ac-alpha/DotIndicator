1f2a35104651b6201bba59901ea13948cbe8e135
ac-alpha <ashutoshchaubey2999@gmail.com>
2018-03-05 23:01:02 +0530

    Simple sample of adding dot indicators in the bottm of a viewpager

---
+++ app/src/main/java/com/example/blindcoder007/dotindicator/SampleFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.blindcoder007.dotindicator;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by ashutoshchaubey on 05/03/18.
+ */
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+
+        super(fm);
+        this.context=context;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position==0){
+            return new OneFragment();
+        }else if(position==1) {
+            return new TwoFragment();
+        }else{
+            return new ThreeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+
+}
